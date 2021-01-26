9c1b6a9f0e6ebe0637a5172a98fa5b1389a48c0b
skynet <epixunknown@protonmail.com>
2021-01-26 15:49:54 +0800

    Added multiple files

---
+++ config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <iostream>
+using namespace std;
+
+string working_dir="data/";
+
+#endif
---
+++ main.cc
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+#include "config.h"
+using namespace std;
+int main(){
+    
+}
